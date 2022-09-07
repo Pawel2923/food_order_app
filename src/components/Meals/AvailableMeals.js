c2bacf5dd75952bc527a5273da82b848fa9f5e72
Paweł <pawelporemba123@gmail.com>
2022-09-07 14:06:36 +0200

    Fix meals fetch handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,31 +6,56 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
-
-  const getData = async () => {
-    const response = await fetch(
-      "https://react-database-course-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
-    );
-
-    if (!response.ok) {
-      throw new Error("Couldn't connect to database");
-    }
-
-    response
-      .json()
-      .then((response) => setMeals(response))
-      .catch((error) => {
-        throw new Error(error);
-      });
-  };
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
-    try {
-      getData();
-    } catch (error) {
-      console.error(error);
-    }
+    const getData = async () => {
+      const response = await fetch(
+        "https://react-database-course-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Couldn't connect to database");
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    getData().catch((error) => {
+      setIsLoading(false);
+      console.error(error.message);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes["meals-loading"]}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes["meals-error"]}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
